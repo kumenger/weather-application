dcd635aea3b0901b0aa07f07e7f4f2921bf87c46
kumenger <44558777+kumenger@users.noreply.github.com>
2023-02-22 00:20:46 -0700

    fixed city not found

--- assests/js/script.js
+++ assests/js/script.js
@@ -7,20 +7,19 @@ var display = document.getElementById("display");
 var openWatherApiKey = "87710827a4c6f11401d8a2d244caad74";
 var cityValue = document.getElementById("cityInput");
 var cities = JSON.parse(localStorage.getItem("cities") || "[]");
-var serchHistoryEL=document.getElementsByClassName('styleList')
-var errorDisaplyEL=document.getElementById('errDisaply')
+var serchHistoryEL = document.getElementsByClassName("styleList");
+var errorDisaplyEL = document.getElementById("errDisaply");
 
-oEL.addEventListener('click',function(e){
-if(e.target.tagname='li'){
-  fetchWather(e.target.innerHTML)
-}
-})
+oEL.addEventListener("click", function (e) {
+  if ((e.target.tagname = "li")) {
+    fetchWather(e.target.innerHTML);
+  }
+});
 
 var isExits = false;
 window.addEventListener("load", () => {
-
   display.style.display = "block";
-  if (cities.length>0) {
+  if (cities.length > 0) {
     for (var elem of cities) {
       var liEl = document.createElement("li");
       liEl.classList.add("styleList");
@@ -28,59 +27,45 @@ window.addEventListener("load", () => {
       liEl.textContent = elem.cityame;
       oEL.appendChild(liEl);
     }
-  
   }
- 
-    navigator.geolocation.getCurrentPosition((p)=>{
-        todayWeather.innerHTML='';
-        weekforcastEL.innerHTML=''
-        let lat = p.coords.latitude;
-        let long = p.coords.longitude;
-        var url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=87710827a4c6f11401d8a2d244caad74&units=imperial`;
-        
-        fetch(url)
-        .then((resp) => resp.json())
-        .then((data) => {
-         
-          var time2 = dayjs.unix(data.list[0].dt);
-          var CitynameDateIcon = document.createElement("h1");
-          CitynameDateIcon.innerHTML = `Your Located in ${data.city.name}(${time2.format(
-            "DD/MM/YYYY"
-          )})`;
-          var tempdoc = document.createElement("h4");
-          var winddoc = document.createElement("h4");
-          var humdoc = document.createElement("h4");
-          var todayImg = document.createElement("img");
-          todayImg.setAttribute(
-            "src",
-            "http://openweathermap.org/img/wn/" +
-              data.list[0].weather[0].icon +
-              "@2x.png"
-          );
-          tempdoc.innerHTML = `Temp: ${data.list[0].main.temp} °F`;
-          winddoc.innerHTML = `Wind: ${data.list[0].wind.speed} MPH`;
-          humdoc.innerHTML = `Humidity: ${data.list[0].main.humidity} %`;
-        
-            todayWeather.appendChild(CitynameDateIcon);
-            todayWeather.appendChild(todayImg);
-            todayWeather.appendChild(winddoc);
-            todayWeather.appendChild(humdoc);
-            todayWeather.appendChild(tempdoc);
-           
-          
-          })
-      
-     
-     
-    })
-  
-   
 
+  navigator.geolocation.getCurrentPosition((p) => {
+    todayWeather.innerHTML = "";
+    weekforcastEL.innerHTML = "";
+    let lat = p.coords.latitude;
+    let long = p.coords.longitude;
+    var url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=87710827a4c6f11401d8a2d244caad74&units=imperial`;
 
-  
-  
+    fetch(url)
+      .then((resp)=>resp.json())
+      .then((data) => {
+        var time2 = dayjs.unix(data.list[0].dt);
+        var CitynameDateIcon = document.createElement("h1");
+        CitynameDateIcon.innerHTML = `Your Located in ${
+          data.city.name
+        }(${time2.format("DD/MM/YYYY")})`;
+        var tempdoc = document.createElement("h4");
+        var winddoc = document.createElement("h4");
+        var humdoc = document.createElement("h4");
+        var todayImg = document.createElement("img");
+        todayImg.setAttribute(
+          "src",
+          "http://openweathermap.org/img/wn/" +
+            data.list[0].weather[0].icon +
+            "@2x.png"
+        );
+        tempdoc.innerHTML = `Temp: ${data.list[0].main.temp} °F`;
+        winddoc.innerHTML = `Wind: ${data.list[0].wind.speed} MPH`;
+        humdoc.innerHTML = `Humidity: ${data.list[0].main.humidity} %`;
+
+        todayWeather.appendChild(CitynameDateIcon);
+        todayWeather.appendChild(todayImg);
+        todayWeather.appendChild(winddoc);
+        todayWeather.appendChild(humdoc);
+        todayWeather.appendChild(tempdoc);
+      });
+  });
 });
-
 
 submit.addEventListener("click", (e) => {
   e.preventDefault();
@@ -88,27 +73,32 @@ submit.addEventListener("click", (e) => {
   display.style.display = "block";
   var val = cityValue.value;
   if (!val) {
-    errorDisaplyEL.style.display='block'
-    errorDisaplyEL.innerHTML='City Required'
+    errorDisaplyEL.style.display = "block";
+    errorDisaplyEL.innerHTML = "City Required";
 
-  setInterval(() => {
-    
-    errorDisaplyEL.style.display='none'
-  }, 2000);
-  } else 
-  fetchWather(val)
- 
-  
-  
-   
+    setInterval(() => {
+      errorDisaplyEL.style.display = "none";
+    }, 2000);
+  } else fetchWather(val);
 });
 
-const fetchWather=(city)=>{
-   var baseurlNow = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${openWatherApiKey}`;
+const fetchWather = (city) => {
+  var baseurlNow = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${openWatherApiKey}`;
 
   fetch(baseurlNow)
-    .then((resp) => resp.json())
+    .then(function (resp) {
+      if (resp.status === 404) {
+        errorDisaplyEL.style.display = "block";
+        errorDisaplyEL.innerHTML = "City not Found!!";
+
+        setInterval(() => {
+          errorDisaplyEL.style.display = "none";
+        }, 2000);
+      } else 
+       return resp.json();
+    })
     .then((data) => {
+      
       var newlat = data.coord.lat;
       var newLon = data.coord.lon;
       var fiveDayBaseUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${newlat}&lon=${newLon}&appid=87710827a4c6f11401d8a2d244caad74&units=imperial`;
@@ -119,20 +109,20 @@ const fetchWather=(city)=>{
           lon: data.coord.lon,
         },
       };
-      var lielhere=document.createElement('li')
-      lielhere.innerHTML=data.name;
+      var lielhere = document.createElement("li");
+      lielhere.innerHTML = data.name;
       lielhere.classList.add("styleList");
-     
-      oEL.appendChild(lielhere)
+
+      oEL.appendChild(lielhere);
       //if previosuly city was  add to local storage not push to array bro
-      if(cities.length>0){
+      if (cities.length > 0) {
         for (var elem of cities) {
           if (elem.cityame === data.name) {
             isExits = true;
           }
         }
       }
-   
+
       if (!isExits) {
         cities.push(obj);
       }
@@ -159,14 +149,14 @@ const fetchWather=(city)=>{
           tempdoc.innerHTML = `Temp: ${data.list[0].main.temp} °F`;
           winddoc.innerHTML = `Wind: ${data.list[0].wind.speed} MPH`;
           humdoc.innerHTML = `Humidity: ${data.list[0].main.humidity} %`;
-          todayWeather.innerHTML=''
-          weekforcastEL.innerHTML=''
-          newh3hold.innerHTML=''
-            todayWeather.appendChild(CitynameDateIcon);
-            todayWeather.appendChild(todayImg);
-            todayWeather.appendChild(winddoc);
-            todayWeather.appendChild(humdoc);
-            todayWeather.appendChild(tempdoc);
+          todayWeather.innerHTML = "";
+          weekforcastEL.innerHTML = "";
+          newh3hold.innerHTML = "";
+          todayWeather.appendChild(CitynameDateIcon);
+          todayWeather.appendChild(todayImg);
+          todayWeather.appendChild(winddoc);
+          todayWeather.appendChild(humdoc);
+          todayWeather.appendChild(tempdoc);
           var weekForcast = data.list;
           var newH3 = document.createElement("h2");
           newH3.innerHTML = "5-day forcast";
@@ -191,22 +181,17 @@ const fetchWather=(city)=>{
             tempdocW.innerHTML = `Temp: ${data.list[i].main.temp} °F`;
             winddocW.innerHTML = `Wind: ${data.list[i].wind.speed} MPH`;
             humdocW.innerHTML = `Humidity: ${data.list[i].main.humidity} %`;
-       
-              weekHolderDiv.appendChild(CitynameDateIconW);
-              weekHolderDiv.appendChild(iconhold);
-              weekHolderDiv.appendChild(tempdocW);
-              weekHolderDiv.appendChild(winddocW);
-              weekHolderDiv.append(humdocW);
-             
-             weekforcastEL.appendChild(weekHolderDiv);
-            
-          
+
+            weekHolderDiv.appendChild(CitynameDateIconW);
+            weekHolderDiv.appendChild(iconhold);
+            weekHolderDiv.appendChild(tempdocW);
+            weekHolderDiv.appendChild(winddocW);
+            weekHolderDiv.append(humdocW);
+
+            weekforcastEL.appendChild(weekHolderDiv);
           }
         })
         .catch((err) => console.log(err));
     })
     .catch((err) => console.log(err));
-}
-
-
-
+};
